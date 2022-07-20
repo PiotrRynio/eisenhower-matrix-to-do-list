9afee9f148b4ec231c252803d1123408400df206
Piotr Rynio <pwrynio@gmail.com>
2022-07-20 13:09:00 +0200

    feat(project): add logo animation

--- src/components/Logo/Logo.styles.tsx
+++ src/components/Logo/Logo.styles.tsx
@@ -4,4 +4,30 @@ export const LogoImage = styled.img`
   display: block;
   height: 38px;
   width: 38px;
+
+  &:hover {
+    animation: shake 2s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+    @keyframes shake {
+      0% {
+        transform: translate3d(0, 0, 0);
+      }
+      5%,
+      45% {
+        transform: translate3d(-1px, 0, 0);
+      }
+      10%,
+      40% {
+        transform: translate3d(2px, 0, 0);
+      }
+      15%,
+      25%,
+      35% {
+        transform: translate3d(-4px, 0, 0);
+      }
+      20%,
+      30% {
+        transform: translate3d(4px, 0, 0);
+      }
+    }
+  }
 `;
